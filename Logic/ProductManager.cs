df89d8c593a91054f0f035402c8cfd3e874fcb77
BMB <bruno.marquez@ucb.edu.bo>
2022-05-21 21:33:29 -0400

    Added Crud Operations

--- Logic/ProductManager.cs
+++ Logic/ProductManager.cs
@@ -4,7 +4,31 @@ using System.Text;
 
 namespace Logic
 {
-    internal class ProductManager
+    internal class ProductManager : IProductManager
     {
+        public List<Product> Products { get; set; }
+
+        public ProductManager()
+        {
+            
+        }
+        
+        public List<Product> GetProducts()
+        {
+            return null;
+        }
+        public Product PostProduct()
+        {
+            return null;
+        }
+        public Product PutProduct()
+        {
+            return null;
+        }
+        public Product DeleteProduct()
+        {
+            return null;
+
+        }
     }
 }

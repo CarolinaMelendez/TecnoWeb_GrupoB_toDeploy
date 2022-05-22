d794714863cfb06f56d9eed6220a553a0fbde716
BMB <bruno.marquez@ucb.edu.bo>
2022-05-21 21:22:06 -0400

    Added ProductsController and Started LogicLayer

---
+++ ProductsInventory/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsInventory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductManager _productManager;
+
+        public ProductsController(IProductManager productManager)
+        {
+            _productManager = productManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult PostProduct()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult PutProduct()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(Product product)
+        {
+            return Ok();
+        }
+    }
+}

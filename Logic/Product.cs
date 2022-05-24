89c19ea5f2e87d7cb3dfd7c7b35a9540c41b0d56
Alfred Garcia <alfred.garcia@ucb.edu.bo>
2022-05-24 10:57:00 -0400

    addition about Product and productManager

--- Logic/Product.cs
+++ Logic/Product.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public string Type { get; set; }
         public string Code { get; set; }
+        public int Price { get; set; }
         public int Stock { get; set; }
     }
 }
--- Logic/ProductManager.cs
+++ Logic/ProductManager.cs
@@ -7,13 +7,15 @@ namespace Logic
     public class ProductManager : IProductManager
     {
         public List<Product> Products { get; set; }
-
+        //public ProductManager(IDbLayer dbLayer)
         public ProductManager()
         {
-            Products = new List<Product>
+            Products = new List<Product>()
             {
-                new Product() { Name = "Polera", Type = "SOCCER", Code = "SOCCER-001",  Stock = 10 },
-                new Product() { Name = "Balon", Type = "BASKET", Code = "BASKET-001",  Stock = 10 }
+                new Product() { Name = "Polera", Type = "SOCCER", Code = "SOCCER-001",  Price = 45, Stock = 100 },
+                new Product() { Name = "Corto", Type = "SOCCER", Code = "SOCCER-002",  Price = 30, Stock = 50 },
+                new Product() { Name = "Tennis", Type = "BASQUET", Code = "BASQUET-001",  Price = 120, Stock = 250 },
+                new Product() { Name = "Balon", Type = "BASKET", Code = "BASKET-002", Price = 50, Stock = 20 },
             };
         }
         
